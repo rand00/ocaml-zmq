ceee3a27d179d05873f553b2a29c4240e63b289f
rand00 <oth.rand@gmail.com>
2025-03-28 13:16:38 +0100

    zmq-deferred/src/Socket: Added retrying after EINTR error from 'zmq_msg_recv' + Readded debug-printing

--- zmq-deferred/src/socket.ml
+++ zmq-deferred/src/socket.ml
@@ -141,7 +141,11 @@ module Make(T: Deferred.T) = struct
           event_loop t
         | exception Unix.Unix_error(Unix.ENOTSOCK, "zmq_getsockopt", "") ->
           Deferred.return ()
-        | exception Unix.Unix_error(Unix.EINTR, "zmq_getsockopt", "") ->
+        | exception Unix.Unix_error(Unix.EINTR, (
+            "zmq_msg_recv"
+          | "zmq_getsockopt"
+        ), "") ->
+          Format.eprintf "DEBUG: ZMQ-lwt: Got EINTR, retrying..\n%!";
           event_loop t
       end
 
