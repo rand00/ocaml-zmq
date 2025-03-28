1e1dd9e72e52ffba84cb9ff463e491a0f288916d
rand00 <oth.rand@gmail.com>
2025-03-28 13:47:58 +0100

    zmq-deferred/src/Socket: Fixed that we shouldn't print EINTR error when EAGAIN

--- zmq-deferred/src/socket.ml
+++ zmq-deferred/src/socket.ml
@@ -166,10 +166,11 @@ module Make(T: Deferred.T) = struct
     let f' mailbox () =
       let res = match f t.socket with
         | v -> Ok v
-        | exception Unix.Unix_error (Unix.EAGAIN, _, _) 
+        | exception Unix.Unix_error (Unix.EAGAIN, _, _) -> 
+          (* Signal try again *)
+          raise Retry
         | exception Unix.Unix_error (Unix.EINTR, "zmq_msg_recv", _) ->
           Format.eprintf "DEBUG: ZMQ-lwt: Got EINTR from 'zmq_msg_recv' - retrying..\n%!";
-          (* Signal try again *)
           raise Retry
         | exception exn -> Error exn
       in
